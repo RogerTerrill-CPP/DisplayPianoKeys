9df4b9d8eae679e257122939ef935d82e420559f
Roger Terrill <roger.terrill@antaira.com>
2016-10-04 10:57:12 -0700

    Initial

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<  "________________________________________\n"
+        <<  "|  | | | |  |  | | | | | |  |  | | | |  |\n"
+        <<  "|  | | | |  |  | | | | | |  |  | | | |  |\n"
+        <<  "|  | | | |  |  | | | | | |  |  | | | |  |\n"
+        <<  "|  |_| |_|  |  |_| |_| |_|  |  |_| |_|  |\n"
+        <<  "|   |   |   |   |   |   |   |   |   |   |\n"
+        <<  "|   |   |   |   |   |   |   |   |   |   |\n"
+        <<  "|___|___|___|___|___|___|___|___|___|___|\n";
+    return 0;
+}
+/*
+Name            : homework3
+Arguements      :
+Return          : 0
+Definition      : A function the displays a piano
+*/
